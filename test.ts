05331b2f203a0020293dc285da53d0f7424bb62f
eliassjogreen <eliassjogreen1@gmail.com>
2020-04-30 20:15:35 +0200

    Update test libs

--- test.ts
+++ test.ts
@@ -1,64 +1,60 @@
 import { assertEquals } from "./test_deps.ts";
 import { prepare, PerpareOptions } from "./mod.ts";
 
-const test = Deno.test;
 const textDecoder = new TextDecoder();
 
-test({
-  name: "prepare",
-  async fn(): Promise<void> {
-    const releaseUrl =
-      "https://github.com/manyuanrong/deno-plugin-prepare/releases/download/plugin_bins";
+async function testPrepare() {
+  const releaseUrl =
+    "https://github.com/eliassjogreen/deno-plugin-prepare/releases/download/plugin_bins";
 
-    const pluginOptions: PerpareOptions = {
-      name: "test_plugin",
-      printLog: true,
-      urls: {
-        darwin: `${releaseUrl}/libtest_plugin.dylib`,
-        windows: `${releaseUrl}/test_plugin.dll`,
-        linux: `${releaseUrl}/libtest_plugin.so`,
-      },
-    };
-    const plugin = await prepare(pluginOptions);
-    //@ts-ignore
-    const { testSync } = Deno.core.ops();
+  const pluginOptions: PerpareOptions = {
+    name: "test_plugin",
+    printLog: true,
+    urls: {
+      darwin: `${releaseUrl}/libtest_plugin.dylib`,
+      windows: `${releaseUrl}/test_plugin.dll`,
+      linux: `${releaseUrl}/libtest_plugin.so`,
+    },
+  };
+  const pluginId = await prepare(pluginOptions);
+  //@ts-ignore
+  const { testSync } = Deno.core.ops();
 
-    //@ts-ignore
-    const response = Deno.core.dispatch(
-      testSync,
-      new Uint8Array([116, 101, 115, 116]),
-      new Uint8Array([116, 101, 115, 116]),
-    )!;
+  //@ts-ignore
+  const response = Deno.core.dispatch(
+    testSync,
+    new Uint8Array([116, 101, 115, 116]),
+    new Uint8Array([116, 101, 115, 116]),
+  )!;
 
-    assertEquals(textDecoder.decode(response), "test");
-  },
-});
+  assertEquals(textDecoder.decode(response), "test");
+}
 
-test({
-  name: "prepare local",
-  async fn(): Promise<void> {
-    const releaseUrl = "file://./test_bins";
+async function testPrepareFromLocal() {
+  const releaseUrl = "file://./test_bins";
 
-    const pluginOptions: PerpareOptions = {
-      name: "test_plugin",
-      printLog: true,
-      checkCache: false,
-      urls: {
-        darwin: `${releaseUrl}/libtest_plugin.dylib`,
-        windows: `${releaseUrl}/test_plugin.dll`,
-        linux: `${releaseUrl}/libtest_plugin.so`,
-      },
-    };
-    const plugin = await prepare(pluginOptions);
-    //@ts-ignore
-    const { testSync } = Deno.core.ops();
+  const pluginOptions: PerpareOptions = {
+    name: "test_plugin",
+    printLog: true,
+    checkCache: false,
+    urls: {
+      darwin: `${releaseUrl}/libtest_plugin.dylib`,
+      windows: `${releaseUrl}/test_plugin.dll`,
+      linux: `${releaseUrl}/libtest_plugin.so`,
+    },
+  };
+  const pluginId = await prepare(pluginOptions);
+  //@ts-ignore
+  const { testSync } = Deno.core.ops();
 
-    //@ts-ignore
-    const response = Deno.core.dispatch(
-      testSync,
-      new Uint8Array([116, 101, 115, 116]),
-      new Uint8Array([116, 101, 115, 116]),
-    )!;
-    assertEquals(textDecoder.decode(response), "test");
-  },
-});
+  //@ts-ignore
+  const response = Deno.core.dispatch(
+    testSync,
+    new Uint8Array([116, 101, 115, 116]),
+    new Uint8Array([116, 101, 115, 116]),
+  )!;
+  assertEquals(textDecoder.decode(response), "test");
+}
+
+await testPrepare();
+await testPrepareFromLocal();
