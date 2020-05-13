e0ed53702e2cb9cabc338b23db372e88ce5c474d
Andreas Hippler <andreas@tabaro.de>
2020-05-13 02:25:54 +0200

    fix tests and ci cache

--- test.ts
+++ test.ts
@@ -1,20 +1,31 @@
-import { delay } from "./test_deps.ts";
+import { exists } from "./deps.ts";
+import { assert, serve, serveFile, resolve } from "./test_deps.ts";
 
-const fileServer = Deno.run({
-  cmd: [
-    Deno.execPath(),
-    "run",
-    "--allow-read",
-    "--allow-net",
-    "https://deno.land/std@v0.50.0/http/file_server.ts",
-  ],
-});
+const pluginDir = ".deno_plugins";
+
+async function cleanCache() {
+  if (await exists(pluginDir)) {
+    return Deno.remove(pluginDir, { recursive: true });
+  }
+}
+
+async function pluginIsCached(): Promise<boolean> {
+  if (!await exists(pluginDir)) {
+    return false;
+  }
+  for (const _ of Deno.readDirSync(pluginDir)) {
+    return true;
+  }
+  return false;
+}
 
 Deno.test("testPrepareLocal", async () => {
-  const p = Deno.run({
+  await cleanCache();
+
+  let p = Deno.run({
     cmd: [
       "deno",
-      "test",
+      "run",
       "--allow-read",
       "--allow-write",
       "--allow-plugin",
@@ -22,25 +33,90 @@ Deno.test("testPrepareLocal", async () => {
       "./tests/local.ts",
     ],
   });
-  await p.status();
+
+  let status = await p.status();
+  assert(status.success);
+  assert(await pluginIsCached());
+  p.close();
+
+  p = Deno.run({
+    cmd: [
+      "deno",
+      "run",
+      "--allow-read",
+      "--allow-write",
+      "--allow-plugin",
+      "--unstable",
+      "./tests/local.ts",
+    ],
+  });
+
+  status = await p.status();
+  assert(status.success);
+
   p.close();
 });
+
+function startServer(addr: string) {
+  const server = serve("127.0.0.1:4500");
+
+  (async () => {
+    for await (const request of server) {
+      const response = await serveFile(
+        request,
+        resolve(`.${request.url}`),
+      );
+      const file = response.body as Deno.File;
+      await request.respond(response);
+      file.close();
+    }
+  })();
+
+  return server;
+}
 
 Deno.test("testPrepareRemote", async () => {
-  await delay(1000);
-  const p = Deno.run({
+  await cleanCache();
+
+  const address = "localhost:4500";
+  const server = startServer(address);
+
+  let p = Deno.run({
     cmd: [
       "deno",
-      "test",
+      "run",
       "--allow-read",
       "--allow-write",
       "--allow-plugin",
+      "--allow-net",
       "--unstable",
-      "./tests/local.ts",
+      "./tests/remote.ts",
+      address,
     ],
   });
-  await p.status();
-  p.close();
-});
 
-fileServer.close();
+  let status = await p.status();
+  assert(status.success);
+  assert(await pluginIsCached());
+  p.close();
+
+  p = Deno.run({
+    cmd: [
+      "deno",
+      "run",
+      "--allow-read",
+      "--allow-write",
+      "--allow-plugin",
+      "--allow-net",
+      "--unstable",
+      "./tests/remote.ts",
+      address,
+    ],
+  });
+
+  status = await p.status();
+  assert(status.success);
+
+  p.close();
+  server.close();
+});
