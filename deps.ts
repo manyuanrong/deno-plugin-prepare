e61639f61c58b83554e4d70ebe6414441d044c46
Enok <416828041@qq.com>
2020-05-14 09:22:10 +0800

    Bump to std@v0.50.0

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { exists } from "https://deno.land/std@v1.0.0-rc1/fs/exists.ts";
-export * as log from "https://deno.land/std@v1.0.0-rc1/log/mod.ts";
-export * as path from "https://deno.land/std@v1.0.0-rc1/path/mod.ts";
+export { exists } from "https://deno.land/std@v0.50.0/fs/exists.ts";
+export * as log from "https://deno.land/std@v0.50.0/log/mod.ts";
+export * as path from "https://deno.land/std@v0.50.0/path/mod.ts";
 
-export { Hash } from "https://deno.land/x/checksum@1.3.0/mod.ts";
+export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
