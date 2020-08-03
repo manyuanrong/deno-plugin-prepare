269c007b1e7c8fe4561e46028d2c8ee54b3dba9c
avalero <alberto.valero@bq.com>
2020-08-03 23:30:51 +0200

    fix std import link

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { exists } from "https://deno.land/std@v0.61.0/fs/exists.ts";
-export * as log from "https://deno.land/std@v0.61.0/log/mod.ts";
-export * as path from "https://deno.land/std@v0.61.0/path/mod.ts";
+export { exists } from "https://deno.land/std@0.61.0/fs/exists.ts";
+export * as log from "https://deno.land/std@0.61.0/log/mod.ts";
+export * as path from "https://deno.land/std@0.61.0/path/mod.ts";
 
 export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
