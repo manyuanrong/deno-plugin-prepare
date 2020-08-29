62e232ae40f628ac372bbfdb3fd15ac5e961d1f9
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2020-08-29 17:41:09 +0200

    fix: update std to 0.67.0

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
-export { exists } from "https://deno.land/std@0.63.0/fs/exists.ts";
-export * as log from "https://deno.land/std@0.63.0/log/mod.ts";
-export * as path from "https://deno.land/std@0.63.0/path/mod.ts";
+export { exists } from "https://deno.land/std@0.67.0/fs/exists.ts";
+export * as log from "https://deno.land/std@0.67.0/log/mod.ts";
+export * as path from "https://deno.land/std@0.67.0/path/mod.ts";
 
 export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
